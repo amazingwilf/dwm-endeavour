b7a86ae2362675180283160df3bff75a49c501a6
David Elliott <david@starjump.org>
2023-12-21 19:36:52 +0000

    tweaks

--- config.def.h
+++ config.def.h
@@ -33,7 +33,7 @@ static const char *colors[][3]      = {
 	[SchemeLtSymbol]	= { col_blue,	col_gray1,	col_gray1 },
 	[SchemeTagsNorm]	= { col_gray2,	col_gray1,	col_gray1 },
 	[SchemeTagsOcc]		= { col_blue,	col_gray1,	col_gray1 },
-	[SchemeTagsSel]		= { col_gray4,	col_cyan,	col_gray1 },
+	[SchemeTagsSel]		= { col_gray4,	col_gray1,	col_gray1 },
 	[SchemeTitleNorm]	= { col_gray3,	col_gray1,	col_gray1 },
 	[SchemeTitleSel]	= { col_blue,	col_gray1,	col_gray1 },
 };
@@ -54,8 +54,13 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
